fed16b46a103618cfeef87610b91f7b12fecee03
Thorekt <robinory77@gmail.com>
2022-10-07 11:01:00 +0200

    Fix piece jointe jpg, jpeg et png

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -15,15 +15,19 @@ export default class Login {
     const formAdmin = this.document.querySelector(`form[data-testid="form-admin"]`)
     formAdmin.addEventListener("submit", this.handleSubmitAdmin)
   }
+
   handleSubmitEmployee = e => {
     e.preventDefault()
+
     const user = {
       type: "Employee",
       email: e.target.querySelector(`input[data-testid="employee-email-input"]`).value,
       password: e.target.querySelector(`input[data-testid="employee-password-input"]`).value,
       status: "connected"
     }
+
     this.localStorage.setItem("user", JSON.stringify(user))
+
     this.login(user)
       .catch(
         (err) => this.createUser(user)
@@ -39,13 +43,16 @@ export default class Login {
 
   handleSubmitAdmin = e => {
     e.preventDefault()
+
     const user = {
       type: "Admin",
       email: e.target.querySelector(`input[data-testid="admin-email-input"]`).value,
       password: e.target.querySelector(`input[data-testid="admin-password-input"]`).value,
       status: "connected"
     }
+
     this.localStorage.setItem("user", JSON.stringify(user))
+    
     this.login(user)
       .catch(
         (err) => this.createUser(user)
