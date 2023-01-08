d0fa5e10ce74ceec937b5ea581daa9d11fb97c7b
Thorekt <robinory77@gmail.com>
2023-01-08 23:32:38 +0100

    newbill check if the page show itself

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -2,17 +2,35 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
-import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
+import { screen, waitFor } from '@testing-library/dom';
+import { localStorageMock } from '../__mocks__/localStorage.js';
+import NewBillUI from '../views/NewBillUI.js';
+import NewBill from '../containers/NewBill.js';
+import { ROUTES, ROUTES_PATH } from '../constants/routes';
 
+import router from '../app/Router.js';
 
-describe("Given I am connected as an employee", () => {
-  describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
-    })
-  })
-})
+describe('Given I am connected as an employee', () => {
+  describe('When I am on NewBill Page', () => {
+    test('Then, it should show the page', async () => {
+      Object.defineProperty(window, 'localStorage', {
+        value: localStorageMock,
+      });
+      window.localStorage.setItem(
+        'user',
+        JSON.stringify({
+          type: 'Employee',
+        })
+      );
+
+      const root = document.createElement('div');
+      root.setAttribute('id', 'root');
+      document.body.append(root);
+      router();
+      window.onNavigate(ROUTES_PATH.NewBill);
+      await waitFor(() => screen.getByTestId('form-new-bill'));
+      const formNewBill = screen.getByTestId('form-new-bill');
+      expect(formNewBill).toBeTruthy();
+    });
+  });
+});
